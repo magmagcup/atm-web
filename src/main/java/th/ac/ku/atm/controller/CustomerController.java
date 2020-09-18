cb56666918819e2663a90b36410455182bfeb965
magmagcup <sirawich.d@ku.th>
2020-09-18 09:46:58 +0700

    Finish hash

--- src/main/java/th/ac/ku/atm/controller/CustomerController.java
+++ src/main/java/th/ac/ku/atm/controller/CustomerController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
@@ -22,9 +21,9 @@ public class CustomerController {
 
     @GetMapping
     public String getCustomerPage(Model model) {
-//        customers.add(new Customer(1, "Peter", 1234));
-//        customers.add(new Customer(2, "Nancy", 2345));
-//        customers.add(new Customer(3, "Rick", 3456));
+//        customers.add(new Customer(1, "Peter", "1234"));
+//        customers.add(new Customer(2, "Nancy", "2345"));
+//        customers.add(new Customer(3, "Rick", "3456"));
         model.addAttribute("allCustomers", customerService.getCustomers());
         return "customer";
     }
---
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -0,0 +1,36 @@
+package th.ac.ku.atm.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import th.ac.ku.atm.model.Customer;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private List<Customer> customers = new ArrayList<>();
+
+    @PostConstruct
+    public void postConstruct() {
+        this.customers = new ArrayList<>();
+    }
+
+    public void createCustomer(Customer customer) {
+        String hashPin = hash(customer.getPin());
+        customer.setPin(hashPin);
+        this.customers.add(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(this.customers);
+    }
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
+    }
+
+}
